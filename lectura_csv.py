35f923be2604866daf72e7a6c85b1b3b64cd555b
ericheresi <eheresimedina@gmail.com>
2023-01-17 17:52:23 +0100

    lectura del fichero csv, devuelve lista de listas

--- lectura_csv.py
+++ lectura_csv.py
@@ -1,14 +1,8 @@
 import csv
-import os.path
 
 
 def lectura_csv():
-    print(os.path.isfile("vgsales.csv"))
     with open('vgsales.csv') as file:
         print("leido")
         csv_reader = csv.reader(file)
-        for row in csv_reader:
-            print(row)
-
-
-lectura_csv()
+        return csv_reader
