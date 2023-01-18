12c0ae79f88a08f4f10059bc5b7ad1c4740d3d3e
angelocho64 <angelocho64@gmail.com>
2023-01-18 09:10:55 +0100

    Prueba de calidad de lectura_csv

--- lectura_csv.py
+++ lectura_csv.py
@@ -2,8 +2,15 @@ import csv
 
 
 def lectura_csv():
-    file = open('vgsales.csv')
-    print("leido")
-    csv_reader = csv.reader(file)
-    file.close()
-    return csv_reader
+
+    try:
+        csvfile = "vgsales.csv"
+        file = open(csvfile)
+        print("leido")
+        csv_reader = csv.reader(file)
+        file.close()
+        return csv_reader
+    except FileNotFoundError:
+        print("Error, no se encuentra el fichero", csvfile)
+    except Exception:
+        print("Error desconocido")
