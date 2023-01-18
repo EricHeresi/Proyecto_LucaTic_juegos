dfa690e7a2d0809da9528f7f98d3608148e226cd
Angel Fernandez <angel@ed.team>
2023-01-18 08:56:31 +0100

    Creado fichero menu.py

---
+++ menu.py
@@ -0,0 +1,20 @@
+def menu_principal():
+    print("MENU")
+    print("1. Dar un juego de alta")
+    print("2. Listar juegos de género plataformas")
+    print("3. Listar todos los juegos")
+    print("4. Listar juegos para consolas de Nintendo")
+    print("5. Listar editores")
+    print("6. Listar los 5 juegos más vendidos del mundo")
+    print("7. Listar juegos del siglo XX")
+    print("8. Editar un juego")
+    print("9. Borrar un juegos")
+    print("10. Listar todos los juegos")
+    print("11. Listar juegos por género")
+    print("12. Listar juegos con ventas por encima de la media")
+    print("13. Listar juegos lanzados en años pares")
+
+
+def pedir_opcion():
+    numero = int(input("Seleccione una opcion: "))
+    return numero
